486c75c4263ef38fdbaea9dfb4fe3a346eacf9f8
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2023-03-09 13:45:59 +0100

    add restart password controller and routes

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -3,6 +3,14 @@ import Records from '../models/Records.js';
 import bcrypt from 'bcrypt';
 import validator from 'validator';
 import asyncHandler from 'express-async-handler';
+import jwt from 'jsonwebtoken';
+// import {
+// 	transporter,
+// 	passwordResetRequestMailTemplate,
+// 	passwordChangeConfirmationMailTemplate,
+// } from '../mailService.js';
+
+import nodemailer from 'nodemailer';
 
 const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 const passwordRegex =
@@ -81,7 +89,7 @@ export const updateUser = asyncHandler(async (req, res, next) => {
 	const user = await User.findOne(req.user);
 	if (password !== '') {
 		// Compare old password
-		const match = await bcrypt.compare(password, user.password);
+		const match = bcrypt.compare(password, user.password);
 		if (!match) {
 			return res.status(400).json({
 				message: 'Brak autoryzacji, niepoprawne hasło.',
@@ -130,4 +138,100 @@ export const updateUser = asyncHandler(async (req, res, next) => {
 		await user.save();
 		return res.status(200).json({ message: 'Email został zmieniony.' });
 	}
+});
+
+//@desc Reset password and send email with link for create new password
+//@route POST /users/resetpassword
+//@access Public
+export const resetPassword = asyncHandler(async (req, res) => {
+	const { email } = req.body;
+
+	const existingUser = await User.findOne({ email });
+	if (!existingUser)
+		return res.status(404).json({
+			message: 'Użytkownik o podanym e-mailu nie został odnaleziony.',
+		});
+	// generate reset token
+	const token = jwt.sign(
+		{ id: existingUser._id.toString() },
+		process.env.ACCESS_TOKEN_SECRET,
+		{
+			expiresIn: '15min',
+		}
+	);
+	// link for testing
+	// const link = `http://localhost:5173/resetpassword/#access_token=${token}`;
+	const link = `https://gym-app-pi-three.vercel.app/resetpassword/#access_token=${token}`;
+
+	// send email with reset link
+	const transporter = nodemailer.createTransport({
+		service: 'gmail',
+		secure: false,
+		auth: {
+			user: process.env.EMAIL,
+			pass: process.env.EMAIL_PASSWORD,
+		},
+		tls: {
+			rejectUnauthorized: false,
+		},
+	});
+	const mailOptions = {
+		from: process.env.EMAIL,
+		to: email,
+		subject: 'Reset hasła w aplikacji Menadżer Treningu',
+		text:
+			'Witaj! Otrzymałeś ten e-mail, ponieważ złożyłeś prośbę o zresetowanie hasła. Kliknij na poniższy link, aby przejść do strony resetowania hasła. Ważność linku wygaśnie po 15 minutach. ' +
+			link,
+	};
+	transporter.sendMail(mailOptions, (error, info) => {
+		if (error) {
+			res.status(500).json({
+				message:
+					'Wystąpił problem podczas wysyłania emaila resetującego hasło.',
+			});
+		} else {
+			res.status(200).json({
+				message: `Email resetujący hasło został wysłany na adres ${email}.`,
+			});
+		}
+	});
+});
+
+//@desc Create new password for user who forgot old password
+//@route PATCH /users/resetpassword
+//@access Public
+export const newPassword = asyncHandler(async (req, res) => {
+	const { token, password } = req.body;
+
+	let userID;
+
+	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+		if (err) {
+			// when something go wrong on decoding
+			return res
+				.status(419)
+				.json({ message: 'Token jest niepoprawny lub starcił ważność' });
+		} else {
+			userID = decoded.id;
+		}
+	});
+	// when decoding is successful
+	const user = await User.findOne({ _id: userID });
+
+	if (!user) {
+		return res.status(400).json({
+			error: 'Uzytkownik o podanej nazwie już istnieje.',
+		});
+	}
+	if (!passwordRegex.test(password)) {
+		return res.status(400).json({
+			message:
+				'Hasło musi składać się z 8 znaków i zawierać jedną cyfre oraz znak specjalny.',
+		});
+	}
+	const hashedPassword = await bcrypt.hash(password, saltRounds);
+	user.password = hashedPassword;
+	await user.save();
+
+	return res.status(200).json({ message: 'Hasło zostało zmienione.' });
 });
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,8 +1,14 @@
 import express from 'express';
 const router = express.Router();
-import { createNewUser, updateUser } from '../controllers/usersControllers.js';
+import {
+	createNewUser,
+	updateUser,
+	resetPassword,
+	newPassword,
+} from '../controllers/usersControllers.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 router.route('/').post(createNewUser).patch(protect, updateUser);
+router.route('/reset').post(resetPassword).patch(newPassword);
 
 export default router;
